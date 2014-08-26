1ff28fd3d040b530287ae77eee366912a36a841d
Johannes Harth <166_MMX@gmx.net>
2014-08-26 15:06:02 +0200

    stream -> reader

--- src/main/scripts/foo.groovy
+++ src/main/scripts/foo.groovy
@@ -2,13 +2,13 @@ import com.initvoid.jconfig.PreProcessor
 
 def url = 'https://raw.githubusercontent.com/torvalds/linux/master/arch/x86/Kconfig'.toURL()
 def file = new File('/Users/jharth/IdeaProjects/kconfig-to-xml/src/main/script/res/arch_mips_Kconfig.cache')
-//BufferedInputStream bufferedInputStream = url.newInputStream()
-BufferedInputStream bufferedInputStream = file.newInputStream()
+//Reader reader = url.newReader()
+Reader reader = file.newReader()
 
-ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()
+StringWriter writer = new StringWriter()
 
-PreProcessor.process(bufferedInputStream, byteArrayOutputStream)
+PreProcessor.process(reader, writer)
 
-def result = byteArrayOutputStream.toString()
+def result = writer.toString()
 
 'nop'
